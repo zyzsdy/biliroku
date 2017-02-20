acccc96e496c48a9c90799af90d2dcaad0c8743d
Ted Zyzsdy <zyzsdy@gmail.com>
2017-02-20 13:30:11 +0800

    发布1.4.5版本

--- BiLiRoku/Version.cs
+++ BiLiRoku/Version.cs
@@ -10,11 +10,11 @@ namespace BiliRoku
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     internal static class Ver
     {
-        public const string VER = "1.4.4";
-        public const string DATE = "(2017-1-8)";
-        public const string DESC = "修复了长时间录制CPU占用较高的问题。\nby lautis0503\n\n关闭原有自动更新模块，增加检查更新功能。";
+        public const string VER = "1.4.5";
+        public const string DATE = "(2017-2-20)";
+        public const string DESC = "修复等待开播录制时可能的闪退。";
         public static readonly string OS_VER = "(" + WinVer.SystemVersion.Major + "." + WinVer.SystemVersion.Minor + "." + WinVer.SystemVersion.Build + ")";
-        public static readonly string UA = "FeelyBlog/1.1 (zyzsdy@foxmail.com) BiliRoku/1.4.4 " + OS_VER + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36";
+        public static readonly string UA = "FeelyBlog/1.1 (zyzsdy@foxmail.com) BiliRoku/1.4.5 " + OS_VER + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36";
     }
 
     // 检查更新
